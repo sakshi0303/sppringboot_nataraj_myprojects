535ef9e9aa642fcdbb4730c22c33869f99df5455
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2025-02-03 15:56:31 -0800

    Add files via upload

---
+++ Proac-1/src/main/java/com/nt/Proac1Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proac1Application.class, args);
+	}
+
+}
